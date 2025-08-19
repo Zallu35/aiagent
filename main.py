12e71ff15af404e5d558a8d72aa1cceaf581f6bb
Zallu35 <s14.sboss@gmail.com>
2025-08-19 00:07:45 -0400

    Added --verbose command and setup calculator app for testing

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
-from dotenv import load_dotenv
 import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-from google import genai
 
 client = genai.Client(api_key=api_key)
-
+args=sys.argv[1:]
+user_prompt=" ".join(args)
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
 
 def main():
-    reply = client.models.generate_content(model='gemini-2.0-flash-001', contents=sys.argv)
+    reply = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
     if len(sys.argv) <= 1:
         raise Exception("No prompt provided")
         sys.exit(1)
@@ -19,9 +23,10 @@ def main():
     #print(reply.usage_metadata)
     prompt_tokens = reply.usage_metadata.prompt_token_count
     response_tokens = reply.usage_metadata.candidates_token_count
-
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+    if "--verbose" in args:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 if __name__ == "__main__":
     main()
