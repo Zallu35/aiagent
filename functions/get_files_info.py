c0521406ac300f864928b1afe3daa2a9220ae290
Zallu35 <s14.sboss@gmail.com>
2025-08-21 00:47:17 -0400

    Updated main.py with prompt and tools list

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,5 @@
 import os
-
+from google.genai import types
 
 
 def get_files_info(working_directory, directory="."):
@@ -18,3 +18,16 @@ def get_files_info(working_directory, directory="."):
     output="\n ".join(file_data)
     return f'Result for {directory} directory\n {output}'
     
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,14 +13,39 @@ client = genai.Client(api_key=api_key)
 args=sys.argv[1:]
 user_prompt=" ".join(args)
 messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+system_prompt='''
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+'''
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def main():
-    reply = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
     if len(sys.argv) <= 1:
         raise Exception("No prompt provided")
         sys.exit(1)
-    print(reply.text)
-    #print(reply.usage_metadata)
+
+    reply = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+    )
+
+    if reply.function_calls:
+        for func in reply.function_calls:
+            print(f"Calling function: {func.name}({func.args})")
+            print(func)
+    else:
+        print(reply.text)
+
     prompt_tokens = reply.usage_metadata.prompt_token_count
     response_tokens = reply.usage_metadata.candidates_token_count
     if "--verbose" in args:
